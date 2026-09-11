dcae95c06bcc390d5b25e6106fc7c5b8fc862064
assistatianefernandes76-code <assistatiane.fernandes76@gmail.com>
2026-09-11 13:50:08 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,49 +1,16 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from scipy.spatial import Voronoi
-import random
+import streamlit as st
 
-# Minerais e cores
-minerais = {
-    "HM": "#6d6d6d",
-    "HE": "#c0c0c0",
-    "Magnetita": "#000000",
-    "Goethita": "#b8860b",
-    "Quartzo": "#ffffff"
-}
+st.title("🔬 Microscópio Virtual de Mineralogia")
 
-# Número de grãos
-n_graos = 100
+st.write("Projeto desenvolvido por Tatiane Fernandes")
 
-# Geração aleatória
-np.random.seed(42)
-pontos = np.random.rand(n_graos, 2)
+mineral = st.selectbox(
+    "Selecione um mineral",
+    ["HM", "HE", "Magnetita", "Goethita", "Quartzo"]
+)
 
-# Voronoi
-vor = Voronoi(pontos)
+st.write(f"Mineral selecionado: {mineral}")
 
-fig, ax = plt.subplots(figsize=(8,8))
+if st.button("🎲 Gerar Nova Amostra"):
+    st.success("Nova amostra gerada!")
 
-for region_index in vor.point_region:
-
-    region = vor.regions[region_index]
-
-    if -1 in region or len(region) == 0:
-        continue
-
-    polygon = [vor.vertices[i] for i in region]
-
-    mineral = random.choice(list(minerais.keys()))
-
-    ax.fill(
-        [p[0] for p in polygon],
-        [p[1] for p in polygon],
-        color=minerais[mineral],
-        edgecolor="gray"
-    )
-
-ax.set_xticks([])
-ax.set_yticks([])
-ax.set_aspect("equal")
-
-plt.show()
